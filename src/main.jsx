3a80268a644458c6ad47f83d03993eaf6d423fa7
Tayyab Aslam <thisistayyab@outlook.com>
2025-07-16 18:45:48 +0500

    Update: Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import ThemeCustomization from './themes';
 import ScrollTop from './Components/ScrollTop.jsx';
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')).render(
   
   <StrictMode>
+    <Analytics />
     <ThemeCustomization>
       <ScrollTop>
         <App />
